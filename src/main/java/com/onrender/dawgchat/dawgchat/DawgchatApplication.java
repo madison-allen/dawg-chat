771e4d42b07d9a1a19e7bf9c650be82113536afa
Madison Allen <madison.allen@siu.edu>
2022-10-27 22:55:28 -0500

    Initial commit

--- src/main/java/com/onrender/dawgchat/dawgchat/DawgchatApplication.java
+++ src/main/java/com/onrender/dawgchat/dawgchat/DawgchatApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.sql.*;
 
 @SpringBootApplication
 @RestController
@@ -16,5 +17,24 @@ public class DawgchatApplication {
 	@GetMapping("/")
 	public String healthcheck(){
 		return "200";
+	}
+
+	@GetMapping("/data")
+	public String getData(){
+		String url = System.getenv("INTERNAL_DB_URL");
+		String user = System.getenv("DB_USER");
+		String password = System.getenv("DB_PASSWORD");
+
+		String output = "first name:";
+		try(Connection conn = DriverManager.getConnection(url, user, password);
+			Statement stmt = conn.createStatement();
+			ResultSet rs = stmt.executeQuery("SELECT * FROM students");) {
+			while (rs.next()) {
+				output += rs.getString("first_name");
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return output;
 	}
 }
