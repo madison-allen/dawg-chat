4909d28457cb9a5cd113373507308a68b59ae18e
Madison Allen <madison.allen@siu.edu>
2022-11-15 23:20:06 -0600

    Added courses API endpoints

--- src/main/java/com/onrender/dawgchat/dawgchat/controllers/Courses.java
+++ src/main/java/com/onrender/dawgchat/dawgchat/controllers/Courses.java
@@ -21,16 +21,25 @@ public class Courses {
     public List<String> getEnrolledCourses(@RequestBody Map<String, String> credentials) throws SQLException {
         Statement statement = createStatement();
 
+        //TODO Add error checking to make sure correct credentials are passed
+
         String getEncryptedPasswordQuery = "SELECT enrolled_course FROM students WHERE email = '%s'";
         String formattedQuery = String.format(getEncryptedPasswordQuery,
                 credentials.get("email"));
 
         ResultSet rs = statement.executeQuery(formattedQuery);
 
+        //if there is no results (no enrolled classes), return an empty list
         if(!rs.isBeforeFirst()){
             return new ArrayList<>();
         }
 
+        /*
+         * The result set is moved to the first result (which should be the only result)
+         * and the enrolled courses list is retrieved. There was a
+         * problem where the courses array was null even after checking if
+         * the result set was empty, so that is why the if statement was added.
+         */
         rs.next();
         Array coursesArray = rs.getArray("enrolled_course");
         if(coursesArray == null){
@@ -62,9 +71,11 @@ public class Courses {
     public Map<HttpStatus, String> addCourse(@RequestBody Map<String, String> credentials) throws SQLException {
         Statement statement = createStatement();
 
+        //TODO Add error checking to make sure correct credentials are passed
         String email = credentials.get("email");
         String course_number = credentials.get("course_number");
 
+        //The course is added to the courses list in the student database
         String studentCourseUpdate = "UPDATE students SET enrolled_course = array_append(enrolled_course, '%s') WHERE email = '%s'";
         String formattedUpdate = String.format(studentCourseUpdate,
                 course_number,
@@ -72,6 +83,7 @@ public class Courses {
 
         statement.executeUpdate(formattedUpdate);
 
+        //The student id is retrieved to use in the courses database
         String studentIdQuery = "SELECT student_id FROM students WHERE email = '%s'";
         String formattedIdQuery = String.format(studentIdQuery,
                 credentials.get("email"));
@@ -79,12 +91,17 @@ public class Courses {
         rs.next();
         String student_id = rs.getString("student_id");
 
+        //The student id is placed in the student list in the courses database
         String courseStudentListUpdate = "UPDATE courses SET student_list = array_append(student_list, '%s') WHERE course_number = '%s'";
         String formattedStudentListUpdate = String.format(courseStudentListUpdate,
                 student_id,
                 course_number);
         statement.executeUpdate(formattedStudentListUpdate);
 
+        /*
+         * There probably needs to be some error checking in here,
+         * but for right now it just returns OK unless a SQLException is thrown
+         */
         return Map.of(HttpStatus.OK, "Course Added");
     }
 
@@ -92,9 +109,11 @@ public class Courses {
     public Map<HttpStatus, String> removeCourse(@RequestBody Map<String, String> credentials) throws SQLException {
         Statement statement = createStatement();
 
+        //TODO Add error checking to make sure correct credentials are passed
         String course_number = credentials.get("course_number");
         String email = credentials.get("email");
 
+        //Course is removed from the courses list in the student database
         String removeCourseUpdate = "UPDATE students SET enrolled_course = array_remove(enrolled_course, '%s') WHERE email = '%s'";
         String formattedRemoveUpdate = String.format(removeCourseUpdate,
                 course_number,
@@ -102,6 +121,7 @@ public class Courses {
 
         statement.executeUpdate(formattedRemoveUpdate);
 
+        //The student id is retrieved to use in the courses database
         String studentIdQuery = "SELECT student_id FROM students WHERE email = '%s'";
         String formattedIdQuery = String.format(studentIdQuery,
                 email);
@@ -109,6 +129,7 @@ public class Courses {
         rs.next();
         String student_id = rs.getString("student_id");
 
+        //The student id is removed from the student list in the courses database
         String removeStudentIdUpdate = "UPDATE courses SET student_list = array_remove(student_list, '%s') WHERE course_number = '%s'";
         String formattedRemoveIdUpdate = String.format(removeStudentIdUpdate,
                 student_id,
@@ -116,6 +137,10 @@ public class Courses {
 
         statement.executeUpdate(formattedRemoveIdUpdate);
 
+        /*
+         * There probably needs to be some error checking in here,
+         * but for right now it just returns OK unless a SQLException is thrown
+         */
         return Map.of(HttpStatus.OK, "Course Removed");
     }
 
